c3b5bc26b4b307ad24061361dbf1c05da981d512
Romeithius <6393509+Romeithius@users.noreply.github.com>
2024-09-03 11:41:32 -0500

    Update peer.rb

--- lib/fabric/peer.rb
+++ lib/fabric/peer.rb
@@ -3,13 +3,22 @@ require 'peer/peer_services_pb'
 module Fabric
   class Peer < ClientStub
     def client
-      @client ||= Protos::Endorser::Stub.new(host, creds, **options)
+      @client ||= Protos::Endorser::Stub.new(host, creds)
     end
 
     def send_process_proposal(proposal)
       logging :send_process_proposal_request, proposal.to_h
 
-      response = client.process_proposal proposal
+      # Extract metadata from options or provide default
+      metadata = options[:metadata] || {}
+
+      # If there are additional options like deadlines, add them to metadata
+      if options[:deadline]
+        deadline = (Time.now.to_f + options[:deadline]).to_s
+        metadata['deadline'] = deadline
+      end
+
+      response = client.process_proposal(proposal, metadata: metadata)
 
       logging :send_process_proposal_response, response.to_h
 
